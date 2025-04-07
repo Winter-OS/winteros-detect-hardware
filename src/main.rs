d27dac220f505f2af2f63c13a09b5bd4d967c248
Quentin Horgues <quentin.horgues@ikmail.com>
2025-04-07 18:31:47 +0200

    refactor test

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,17 @@ use system_info::VgaInfo;
 
 fn main() {
     let vga_info = VgaInfo::new().expect("Impossible to read vga info");
-    println!("{:?}", vga_info.has_nvidia_device());
-    println!("{:?}", vga_info.has_nvidia_laptop());
-    println!("{:?}", vga_info.get_nvidia_generation());
+    println!("NVIDIA device ? {}", vga_info.has_nvidia_device());
+    println!(
+        "Laptop with NVIDIA device ? {}",
+        vga_info.has_nvidia_laptop()
+    );
+    println!(
+        "NVIDIA generation device : {}",
+        match vga_info.get_nvidia_generation() {
+            Ok(gen) => gen,
+            Err(_) => "none",
+        }
+    );
     println!("{:#?}", vga_info);
 }
