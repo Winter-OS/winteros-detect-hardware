469d4ebafd892b18533f178669cb77847631868f
Quentin Horgues <quentin.horgues@ikmail.com>
2025-04-10 15:49:39 +0200

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 mod system_info;
 use system_info::VgaInfo;
+
 mod hardware_driver;
+use hardware_driver::HardwareModule;
+
+use std::sync::mpsc;
+use std::thread;
 
 fn main() {
-    let vga_info = VgaInfo::new().expect("Impossible to read vga info");
+    let (tvi, rvi) = mpsc::channel();
+    let (thm, rhm) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vga_info = VgaInfo::new().expect("Impossible to read vga info");
+        tvi.send(vga_info).unwrap();
+    });
+    thread::spawn(move || {
+        let hardware_module = HardwareModule::new().expect("immposible to get available module");
+        thm.send(hardware_module).unwrap();
+    });
+
+    let vga_info = rvi.recv().unwrap();
+    let hardware_module = rhm.recv().unwrap();
+
     println!("NVIDIA device ? {}", vga_info.has_nvidia_device());
     println!(
         "Laptop with NVIDIA device ? {}",
@@ -19,10 +38,16 @@ fn main() {
     println!("{:#?}", vga_info);
 
     println!(
-        "{:#?}",
-        match hardware_driver::get_git_tree("NixOS", "nixos-hardware") {
-            Ok(ret) => ret,
-            Err(err) => panic!("{}", err),
-        }
+        "Architechture Poenix ? {}",
+        vga_info.match_archtecture_codename("phoenix")
     );
+
+    println!("Ambiant light sensor ? {}", system_info::has_iio_device());
+    println!(
+        "Fingerprint sensor ? {}",
+        system_info::has_fingerprint_device()
+    );
+    println!("{:#?}", system_info::ComputerInfo::new().unwrap());
+
+    println!("Available module : {:#?}", hardware_module);
 }
