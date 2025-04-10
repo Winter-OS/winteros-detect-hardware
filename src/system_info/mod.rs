d920b871d91ae793ef5dfee17aeed4f30905e176
Quentin Horgues <quentin.horgues@ikmail.com>
2025-04-10 15:47:18 +0200

    add fingerprint and ambiant light detection

--- src/system_info/mod.rs
+++ src/system_info/mod.rs
@@ -1,2 +1,41 @@
 pub mod vga_info;
 pub use vga_info::VgaInfo;
+
+pub mod computer_info;
+pub use computer_info::ComputerInfo;
+
+use std::path::Path;
+use std::process::Command;
+
+pub fn has_iio_device() -> bool {
+    let path = Path::new("/sys/bus/iio/devices");
+    if path.exists()
+        && path.is_dir()
+        && match path.read_dir() {
+            Ok(read_dir) => read_dir,
+            Err(_) => return false,
+        }
+        .next()
+        .is_some()
+    {
+        return true;
+    }
+    return false;
+}
+
+pub fn has_fingerprint_device() -> bool {
+    let output = match Command::new("lsusb").output() {
+        Ok(out) => out,
+        Err(_) => return false,
+    };
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let lines: Vec<&str> = stdout.trim().split('\n').collect();
+
+    for line in lines {
+        if line.to_lowercase().contains("fingerprint") {
+            return true;
+        }
+    }
+    return false;
+}
