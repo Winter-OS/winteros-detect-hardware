4ab85afeff95b2e5e3f1ebd7267b4d64fe14045a
qhorgues <quentin.horgues@outlook.fr>
2025-08-21 10:34:38 +0200

    add flake in project

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        naersk.url = "github:nix-community/naersk";
+    };
+
+    outputs = { self, nixpkgs, naersk }:
+    let
+        supportedSystems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "i686-linux"
+        ];
+        forAllSystems = f: builtins.listToAttrs (map (system: {
+          name = system;
+          value = f system;
+        }) supportedSystems);
+    in {
+        packages = forAllSystems (system:
+         let
+            pkgs = import nixpkgs { inherit system; };
+            naerskLib = pkgs.callPackage naersk {};
+         in {
+            default = naerskLib.buildPackage {
+                src = ./.;
+                buildInputs = [ pkgs.glib ];
+                nativeBuildInputs = [ pkgs.pkg-config ];
+            };
+            debug = naerskLib.buildPackage {
+                src = ./.;
+                release = false;
+                buildInputs = [ pkgs.glib ];
+                nativeBuildInputs = [ pkgs.pkg-config ];
+            };
+         });
+    };
+}
